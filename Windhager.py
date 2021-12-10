857cd5810bc75143d763bb3286d4d92633457654
tobias <sessl3r@gmail.com>
2021-12-10 15:32:43 +0100

    Windhager: change to use requests.session

--- Windhager.py
+++ Windhager.py
@@ -19,14 +19,16 @@ LOOKUP = 'api/1.0/lookup/'
 ## Fehlerlog
 FEHLERLOG_API = 'InfoWinFehlerlog/api/1.0'
 WINDYNDATA_API = 'WsFUP7030/api/1.0'
-LOOKUP_API = 'api/1.0/lookup/1'
-DATAPOINT_API = 'api/1.0/datapoint/1'
+LOOKUP_API = 'api/1.0/lookup'
+DATAPOINT_API = 'api/1.0/datapoint'
 
 ######### Functions
 
 ## INIT
 def init(ip, user = 'Service', pw = '123'):
-    this.url = 'http://' + ip + '/'
+    this.url = 'http://' + ip
+    this.session = requests.session()
+    this.session.auth = HTTPDigestAuth(this.user, this.pw)
     this.xml = None
     this.init_xml()
 
@@ -35,16 +37,15 @@ def init_xml():
     this.xml = etree.fromstring(resp.text.encode('utf-8'))
 
 def request_get(url, params = None):
-    return requests.get(this.url + url, params = params, auth=HTTPDigestAuth(this.user, this.pw))
+    return this.session.get(f"{this.url}/{url}", params = params)
 
 def request_put(url, data, headers):
-    return requests.put(this.url + url,
+    return this.session.put(f"{this.url}/{url}",
             data=json.dumps(data),
-            headers = headers,
-            auth=HTTPDigestAuth(this.user, this.pw))
+            headers = headers)
 
 def request_delete(url):
-    return requests.delete(this.url + url, auth=HTTPDigestAuth(this.user, this.pw))
+    return this.session.delete(f"{this.url}/{url}")
 
 def get_xml():
     return this.xml
@@ -100,11 +101,11 @@ def getWindyndataReadDapDapId (subnet, node, dap):
 
 ## Lookup API Wrappers
 def get_lookup(obj):
-    return _jget(f"{LOOKUP_API}/{obj}")
+    return _jget(f"{LOOKUP_API}{obj}")
 
 def get_lookup_all():
     ret = []
-    for api in ['15/0', '60/0', '90/0']:
+    for api in ['1/15/0', '1/60/0', '1/90/0']:
         nodes = get_lookup(api)
         for node in nodes:
             tmp = f"{api}/{node['id']}"
@@ -114,7 +115,7 @@ def get_lookup_all():
     return ret
 
 def get_datapoint(p):
-    return _jget('api/1.0/datapoint/' + p)
+    return _jget(DATAPOINT_API + p)
 
 def get_datapoints():
     jget = _jget('api/1.0/datapoints')
